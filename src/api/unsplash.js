07b436b1a852a83cc71b1effeb7a3aaa830541d6
gaby2310 <neagugabriel12@gmail.com>
2020-03-26 01:05:54 +0200

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://api.unsplash.com",
     headers: {
-        Authorization: "Client-ID kTlwItvHXN93mu-Rj2YIsbG9xG52c1GWHVL7VC1aNBs"
+        Authorization: "Client-ID enter_here_API"
     }
 });
